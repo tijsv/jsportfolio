4c7e7335aeb2d4f7edfb8ba70e44693d33f95cdf
Tigroh <tigrodesigns@hotmail.com>
2019-03-03 21:40:08 +0100

    jfs instead of css animations

--- main.js
+++ main.js
@@ -4,15 +4,48 @@
 
         console.log('Starting up ...');
 
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+
+        new JFS([
+            {
+                name: 'fade-in-up',
+                styleStart: 'transform: translateZ(0) translateY(20px) scale(0.7); opacity: 0;',
+                styleEnd: 'transform: translateZ(0) translateY(0) scale(1); opacity: 1;'
+            },
+            {
+                name: 'fade-in-down',
+                styleStart: 'transform: translateZ(0) translateY(-20px) scale(0.7); opacity: 0; transform-origin: center top;',
+                styleEnd: 'transform: translateZ(0) translateY(0) scale(1); opacity: 1;'
+            },
+            {
+                name: 'fade-in-left',
+                styleStart: 'transform: translateZ(0) translateX(20px) scale(0.9); opacity: 0; transform-origin: center right;',
+                styleEnd: 'transform: translateZ(0) translateY(0) scale(1); opacity: 1;'
+            },
+            {
+                name: 'fade-in-right',
+                styleStart: 'transform: translateZ(0) translateX(-20px) scale(0.9); opacity: 0; transform-origin: center left;',
+                styleEnd: 'transform: translateZ(0) translateY(0) scale(1); opacity: 1;'
+            },
+            {
+                name: 'slide-out-left',
+                styleStart: 'transform: translateZ(0) translateX(0) !important',
+                styleEnd: 'transform: translateZ(0) translateX(-100%) !important'
+            },
+            {
+                name: 'slide-out-right',
+                styleStart: 'transform: translateZ(0) translateX(0) !important',
+                styleEnd: 'transform: translateZ(0) translateX(100%) !important'
+            },
+        ]);
+
         let introductionModal = document.querySelector('[data-introduction]'),
             lastAnimationSegment = document.querySelector('[data-animation-end]');
 
-        // set display of introduction animation on none after animation ends
-        introductionModal.addEventListener('webkitAnimationEnd', function(e) {
-            if (lastAnimationSegment !== e.target) return;
-            introductionModal.style.display = "none";
-        })
-        introductionModal.addEventListener('animationend', function(e) {
+        // set display of introduction animation to none after animation ends
+        introductionModal.addEventListener('transitionend', function(e) {
             if (lastAnimationSegment !== e.target) return;
             introductionModal.style.display = "none";
         });
